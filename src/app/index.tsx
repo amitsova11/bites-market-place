afabe992ba378e4d5e5cab361ba0eaf14d978efd
Amit Sova <amitsova@Amits-MacBook-Pro-2.local>
2026-06-15 12:09:22 +0300

    add category filter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,51 +1,53 @@
-import { FlatList, StyleSheet, TextInput, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-import { useTheme } from '@/hooks/use-theme';
-import { useEffect, useState } from 'react';
-import { fetchProducts, FetchProductsParams, Product } from './server/server';
+import { useEffect, useRef, useState } from 'react';
+import { categories, fetchProducts, FetchProductsParams, Product } from './server/server';
 
 
 
 
 export default function HomeScreen() {
 
-  const theme = useTheme();
-
-
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const [search, setSearch] = useState("");
 
+  const loadingRef = useRef(false);
+  const hasMoreRef = useRef(true);
+
   const [data, setData] = useState<Product[]>([]);
+  const [category, setCategory] = useState<string>("All categories");
 
   useEffect(() => {
+    hasMoreRef.current = true;
+    setHasMore(true);
     const timeout = setTimeout(() => {
       loadProducts({
         page: 1,
         search,
+        category
       });
     }, 300);
 
-    setHasMore(true)
-
     return () => clearTimeout(timeout);
-  }, [search]);
+  }, [search, category]);
 
+  const loadProducts = async ({page, search, category}: FetchProductsParams) => {
+          if (loadingRef.current || !hasMoreRef.current) return;
 
-const loadProducts = async ({page, search}: FetchProductsParams) => {
-          if (loading || !hasMore) return;
-
+          loadingRef.current = true;
           setLoading(true);
 
           const res = await fetchProducts({
             page: page,
             limit: 4,
-            search
+            search,
+            category
           });
 
           if (page === 1) {
@@ -54,9 +56,11 @@ const loadProducts = async ({page, search}: FetchProductsParams) => {
             setData((prev) => [...prev, ...res.items]);
           }
 
+          hasMoreRef.current = res.hasMore;
           setHasMore(res.hasMore);
           setPage(page || 1);
 
+          loadingRef.current = false;
           setLoading(false);
       }
 
@@ -71,6 +75,23 @@ const loadProducts = async ({page, search}: FetchProductsParams) => {
             placeholder="Search products..."
           />
           <View style={{marginBottom: 10}}/>
+          <View style={{ flexDirection: "row", gap: 10, flexWrap: 'wrap' }}>
+            {categories.map((c) => (
+                <Pressable
+                  key={c}
+                  onPress={() => setCategory(c)}
+                  style={{
+                    padding: 8,
+                    backgroundColor: category === c ? "black" : "lightgray",
+                  }}
+                >
+                <Text style={{ color: category === c ? "white" : "black" }}>
+                  {c}
+                </Text>
+              </Pressable>
+            ))}
+          </View>
+          <View style={{marginBottom: 10}}/>
           <FlatList
             data={data}
             keyExtractor={(item) => item.id}
@@ -78,6 +99,9 @@ const loadProducts = async ({page, search}: FetchProductsParams) => {
               <View key={item.id} style={styles.product}>
                 <ThemedText style={{fontSize: 30}} >{item.label}</ThemedText>
                 <ThemedText >{item.description}</ThemedText>
+                <ThemedText style={{fontStyle: 'italic', color: '#c0c0c0'}}>
+                  Category: {item.category}
+                </ThemedText>
                 <ThemedText >Price: {item.price}$</ThemedText>
                 <ThemedText >Rating: {item.rating}</ThemedText>
                 <ThemedText style={{color: item.stock ? 'green' : 'red' }} >
@@ -85,7 +109,7 @@ const loadProducts = async ({page, search}: FetchProductsParams) => {
                 </ThemedText>
               </View>
       )}
-      onEndReached={() => loadProducts({page: page +1, search})}
+      onEndReached={() => loadProducts({page: page + 1, search, category})}
       onEndReachedThreshold={0.5}
       ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
     />
