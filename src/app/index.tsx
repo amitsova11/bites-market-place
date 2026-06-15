1c03772a4802155751fc1c0dca3d16d17dffb561
Amit Sova <amitsova@Amits-MacBook-Pro-2.local>
2026-06-15 11:54:59 +0300

    add search

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,30 +1,76 @@
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+import { useTheme } from '@/hooks/use-theme';
 import { useEffect, useState } from 'react';
-import { fetchProducts, Product } from './server/server';
+import { fetchProducts, FetchProductsParams, Product } from './server/server';
 
 
 
 
 export default function HomeScreen() {
 
+  const theme = useTheme();
+
+
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
+  const [search, setSearch] = useState("");
 
   const [data, setData] = useState<Product[]>([]);
 
-  useEffect(() =>{ fetchProducts({limit: 4, page}).then((res) => setData(res.items))} 
-  , [])
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      loadProducts({
+        page: 1,
+        search,
+      });
+    }, 300);
+
+    setHasMore(true)
+
+    return () => clearTimeout(timeout);
+  }, [search]);
+
+
+const loadProducts = async ({page, search}: FetchProductsParams) => {
+          if (loading || !hasMore) return;
+
+          setLoading(true);
+
+          const res = await fetchProducts({
+            page: page,
+            limit: 4,
+            search
+          });
+
+          if (page === 1) {
+            setData(res.items);
+          } else {
+            setData((prev) => [...prev, ...res.items]);
+          }
+
+          setHasMore(res.hasMore);
+          setPage(page || 1);
+
+          setLoading(false);
+      }
 
   return (
     <ThemedView style={styles.container}>
       <SafeAreaView style={styles.safeArea}>
         <ThemedView style={{padding: 24, flex: 1}}>
+          <TextInput
+            value={search}
+            onChangeText={setSearch}
+            style={{color: 'white'}}
+            placeholder="Search products..."
+          />
+          <View style={{marginBottom: 10}}/>
           <FlatList
             data={data}
             keyExtractor={(item) => item.id}
@@ -39,23 +85,7 @@ export default function HomeScreen() {
                 </ThemedText>
               </View>
       )}
-      onEndReached={async () => {
-          if (loading || !hasMore) return;
-
-          setLoading(true);
-
-          const res = await fetchProducts({
-            page: page +1,
-            limit: 4,
-          });
-
-          setData((prev) => [...prev, ...res.items]);
-
-          setHasMore(res.hasMore);
-          setPage( page +1);
-
-          setLoading(false);
-      }}
+      onEndReached={() => loadProducts({page: page +1, search})}
       onEndReachedThreshold={0.5}
       ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
     />
